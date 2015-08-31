4c785d78bd133d2f0abd5f1c25b7f6a912b02b5f
jbc218 <jbc218@lehigh.edu>
2015-08-31 13:52:49 +0000

    Added hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+//Jack Circus
+//Hw01
+//Hello Class Java Program
+//
+//  first compile the program
+//      javac HelloClass.java
+//  run the program
+//  java HelloClass
+//
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+        //print the statement with box around it
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-J--B--C--2--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("I spent most of my summer working as a lifeguard.");
+        System.out.println("I also took a trip to Toronto with my friends.");
+    }
+}
